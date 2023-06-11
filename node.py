5ab03cce29e517d0aa2c09106d4b8018830d90a9
Jinamizi <tonnylango@gmail.com>
2023-06-11 20:19:01 +0300

    push 2

--- node.py
+++ node.py
@@ -1,22 +1,29 @@
 class Node:
-    def __init__(self, value):
+
+    def __init__(self, value, left=None, right=None, parent=None):
         self.__value = value
-        self.__left = None
-        self.__right = None
-        self.__root = None
+        self.left = left
+        self.right = right
+        self.parent = parent
 
     def get_value(self):
         return self.__value
+    
+    def append(self, node):
+        if self < node:
+            if self.left:
+                self.left.append(node)
+            else:
+                node.parent = self
+                self.left = node
+        elif self > node:
+            if self.right:
+                self.right.append(node)
+            else:
+                node.parent = self
+                self.right = node
 
-    def get_left(self):
-        return self.__left
-
-    def get_right(self):
-        return self.__right
-
-    def get_root(self):
-        return self.__root
-
+        
     def __str__(self):
         return str(self.__value)
 
---
+++ tree.py
@@ -0,0 +1,37 @@
+from . import Node
+
+class Tree:
+    #take a list of elements, convert them to nodes and connect them
+    def __init__(self, values:list):
+        self.__root #stores the root node
+        self.__count = 0 #stores number of node
+        #TODO convert to for each
+        
+        
+    
+    def get_root(self):
+        return self.__root
+
+    #insert the node into the tree
+    def insert(self, value):
+        if not
+
+    #returns the number of edges from the root node to that particular node
+    def depth(self, value):
+        pass
+
+    #delete the node from the tree
+    def remove(self, value):
+        pass
+
+    #check if the value exist in the tree
+    def search(self, node):
+        pass
+
+    #count total number of nodes
+    def count(self):
+        pass
+
+    #return a sorted iterator
+    def __iter__(self):
+        pass
